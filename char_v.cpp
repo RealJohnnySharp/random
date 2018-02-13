1eee96bd95823806b588791f05b87d89057520d5
Johnny Sharp <johnny@johnnycsharp.com>
2018-02-13 16:33:56 -0500

    Update char_v logic and add comments

--- char_v.cpp
+++ char_v.cpp
@@ -8,9 +8,18 @@ int main() {
 	cout << "Enter height for V: ";
 	cin >> vHeight;
 
-	for (int i = 0; i < vHeight; i++) {
-		for (int j = 0; j < vHeight*2; j++) {
-			if (((i == j || i == ((vHeight - 1) * 2) - j) && i != vHeight - 1) || i == j) {
+	// Height
+	for (int row = 0; row < vHeight; row++) {
+		// Width
+		for (int col = 0; col < vHeight*2; col++) {
+			// - if the row# and col# match or the row# matches the difference of
+			// 	 the total columns and the current column (the current columns'
+			//	 opposite in the current row), and not the last row
+			// - the last row == col is to account for the last row, since it is
+			//	 excluded from the earlier conditions
+			//
+			// If these conditions are met, * is printed; otherwise a space.
+			if (((row == col || row == ((vHeight - 1) * 2) - col) && row != vHeight - 1) || row == col) {
 				cout << "*";
 			} else {
 				cout << " ";
